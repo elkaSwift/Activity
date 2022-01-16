7aa7e16e21c57d783af11e8f779882a994507656
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-17 01:41:44 +0300

    Data output to ActivityLabel adding an activity indicator

--- Activity/ActivityViewController.swift
+++ Activity/ActivityViewController.swift
@@ -13,67 +13,54 @@ enum Link: String {
 
 class ActivityViewController: UIViewController {
     
-    @IBOutlet weak var ActivityLabel: UILabel!
-    @IBOutlet weak var ActivityButton: UIButton!
+    @IBOutlet weak var activityLabel: UILabel!
+    @IBOutlet weak var activityButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var activity: Activity!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ActivityButton.layer.cornerRadius = 10
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func activityButton(_ sender: UIButton) {
         exampleOneButtonPressed()
-    }
-
-    private func successAlert() {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(
-                title: "Success",
-                message: "You can see the results in the Debug aria",
-                preferredStyle: .alert
-            )
+        if ((activity ?? nil) != nil) {
+            activityLabel.text = """
+Activity: \(activity.activity)
+Type: \(activity.type)
+Participants: \(activity.participants)
+Price: \(activity.price)
+link: \(activity.link)
+Key: \(activity.key)
+Accessibility: \(activity.accessibility)
+"""
             
-            let okAction = UIAlertAction(title: "OK", style: .default)
-            alert.addAction(okAction)
-            self.present(alert, animated: true)
+        } else {
+         print("error")
         }
     }
     
-    private func failedAlert() {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(
-                title: "Failed",
-                message: "You can see error in the Debug aria",
-                preferredStyle: .alert
-            )
-            
-            let okAction = UIAlertAction(title: "OK", style: .default)
-            alert.addAction(okAction)
-            self.present(alert, animated: true)
-        }
-    }
-
-
+    func exampleOneButtonPressed() {
+       guard let url = URL(string: Link.exampleOne.rawValue) else { return }
+       
+       URLSession.shared.dataTask(with: url) { data, _, error in
+           guard let data = data else {
+               print(error?.localizedDescription ?? "No error description")
+               return
+           }
+           
+           do {
+               let activity = try JSONDecoder().decode(Activity.self, from: data)
+               self.activity = activity
+               print(activity)
+           } catch {
+               print(error.localizedDescription)
+           }
+           
+       }.resume()
+   }
 }
 
-extension ActivityViewController {
-    private func exampleOneButtonPressed() {
-        guard let url = URL(string: Link.exampleOne.rawValue) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            do {
-                let course = try JSONDecoder().decode(Activity.self, from: data)
-                self.successAlert()
-                print(course)
-            } catch {
-                self.failedAlert()
-                print(error.localizedDescription)
-            }
-            
 
-        }.resume()
-    }
-    
-}
