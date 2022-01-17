2778bdb4fa50951952dd06fed09dced36cd6c742
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-17 03:02:53 +0300

    Dividing the code into parts for easier movement through the code

--- Activity/ActivityViewController.swift
+++ Activity/ActivityViewController.swift
@@ -13,16 +13,14 @@ enum Link: String {
 
 class ActivityViewController: UIViewController {
     
-   
+    //MARK: - IBOutlets
     @IBOutlet weak var activityLabel: UILabel!
     @IBOutlet weak var activityButton: UIButton!
     
-    
-    
+    //MARK: - properties
     var activity: Activity!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         activityButton.layer.cornerRadius = 10
         activityButton.backgroundColor = .clear
@@ -39,8 +37,8 @@ class ActivityViewController: UIViewController {
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
-
     
+    //MARK: - IBActions
     @IBAction func activityButton(_ sender: UIButton) {
         exampleOneButtonPressed()
         if ((activity ?? nil) != nil) {
@@ -58,25 +56,25 @@ accessibility: \(activity.accessibility)
         }
     }
     
+    //MARK: - public methods
     func exampleOneButtonPressed() {
-       guard let url = URL(string: Link.exampleOne.rawValue) else { return }
-       
-       URLSession.shared.dataTask(with: url) { data, _, error in
-           guard let data = data else {
-               print(error?.localizedDescription ?? "No error description")
-               return
-           }
-           
-           do {
-               let activity = try JSONDecoder().decode(Activity.self, from: data)
-               self.activity = activity
-               print(activity)
-           } catch {
-               print(error.localizedDescription)
-           }
-           
-       }.resume()
-   }
+        guard let url = URL(string: Link.exampleOne.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let activity = try JSONDecoder().decode(Activity.self, from: data)
+                self.activity = activity
+                print(activity)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
 
 
