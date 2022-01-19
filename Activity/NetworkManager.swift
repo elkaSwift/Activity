8c7429ea3e29c3114fe3c337379a97eb97ee4b88
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-19 17:34:33 +0300

    Creating a NetworkManager class and a function responsible for requesting to the network and receiving data from the network

--- Activity/NetworkManager.swift
+++ Activity/NetworkManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetchData(url: String, completion: @escaping(Activity) -> Void) {
+        guard let url = URL(string: Link.exampleOne.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let activity = try JSONDecoder().decode(Activity.self, from: data)
+                DispatchQueue.main.async {
+                    completion(activity)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
