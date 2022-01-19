4d73610525445b0eed8a652061494b1439e8c0ba
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-19 17:46:48 +0300

    Clearing the viewDidLoad method adding a separate function for the gradient

--- Activity/ActivityViewController.swift
+++ Activity/ActivityViewController.swift
@@ -17,17 +17,22 @@ class ActivityViewController: UIViewController {
     @IBOutlet weak var activityLabel: UILabel!
     @IBOutlet weak var activityButton: UIButton!
     
-    //MARK: - properties
-    var activity: Activity!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityButton.layer.cornerRadius = 10
         activityButton.backgroundColor = .clear
         activityButton.layer.cornerRadius = 10
         activityButton.layer.borderWidth = 3
         activityButton.layer.borderColor = UIColor.white.cgColor
         
+        getGradient()
+    }
+    
+    //MARK: - IBActions
+    @IBAction func activityButton(_ sender: UIButton) {
+       
+    }
+    
+    private func getGradient() {
         let firstColor = UIColor(red: 58 / 255, green: 17 / 255, blue: 135 / 255, alpha: 1).cgColor
         let secondColor = UIColor(red: 146 / 255, green: 69 / 255, blue: 144 / 255, alpha: 1).cgColor
         let gradientLayer = CAGradientLayer()
@@ -37,44 +42,8 @@ class ActivityViewController: UIViewController {
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-    //MARK: - IBActions
-    @IBAction func activityButton(_ sender: UIButton) {
-        exampleOneButtonPressed()
-        if ((activity ?? nil) != nil) {
-            activityLabel.text = """
-activity: \(activity.activity)
-type: \(activity.type)
-participants: \(activity.participants)
-price: \(activity.price)
-link: \(activity.link)
-key: \(activity.key)
-accessibility: \(activity.accessibility)
-"""
-        } else {
-            print("error")
-        }
-    }
-    
-    //MARK: - public methods
-    func exampleOneButtonPressed() {
-        guard let url = URL(string: Link.exampleOne.rawValue) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            do {
-                let activity = try JSONDecoder().decode(Activity.self, from: data)
-                self.activity = activity
-                print(activity)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }.resume()
-    }
 }
+    
+
 
 
