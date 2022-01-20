62338ca8463b6d3cc5732a6d88313a58a7b6b377
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-20 21:34:35 +0300

    Taking out manual parsing to the NetworkManager service

--- Activity/NetworkManager.swift
+++ Activity/NetworkManager.swift
@@ -12,15 +12,33 @@ class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
+    func fetchAlamofire(completion: @escaping(Activity) -> Void) {
+        AF.request(Link.exampleOne.rawValue, method: .get)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    guard let activitysData = value as? [String: Any] else { return }
+                    let activity = Activity(activityData: activitysData)
+                    DispatchQueue.main.async {
+                        completion(activity)
+                    }
+                    print(activity)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
     func fetchData(url: String, completion: @escaping(Activity) -> Void) {
         guard let url = URL(string: Link.exampleOne.rawValue) else { return }
-        
+
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            
+
             do {
                 let activity = try JSONDecoder().decode(Activity.self, from: data)
                 DispatchQueue.main.async {
